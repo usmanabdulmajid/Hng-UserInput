5599a0afd144dc131113675eaee32909548dfa64
usmanabdulmajid <abstractmajid@gmail.com>
2021-08-22 04:20:53 -0700

    update

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:simple_gradient_text/simple_gradient_text.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,17 +7,27 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   TextEditingController editingController;
+  AnimationController animationController;
+  Animation<double> animation;
   String displayText = '';
 
   @override
   void initState() {
     editingController = TextEditingController();
+    animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    animation = CurvedAnimation(
+      parent: animationController,
+      curve: Curves.elasticInOut,
+    );
+
     super.initState();
   }
 
   @override
   void dispose() {
     editingController.dispose();
+    animationController.dispose();
     super.dispose();
   }
 
@@ -35,93 +44,115 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
         ),
       ),
       body: SafeArea(
-        child: Container(
-          width: _size.width,
-          height: _size.height,
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            //mainAxisAlignment: MainAxisAlignment.center,
-
-            children: [
-              SizedBox(
-                height: _size.height * 0.20,
-              ),
-              Text(
-                'Enter Text',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'RobotoMono',
+        child: SingleChildScrollView(
+          physics: NeverScrollableScrollPhysics(),
+          child: Container(
+            width: _size.width,
+            height: _size.height,
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: _size.height * 0.20,
                 ),
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              Container(
-                width: _size.width,
-                height: 50.0,
-                padding: EdgeInsets.only(left: 10.0),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4.0),
-                    border: Border.all(
-                      color: Colors.black,
-                    )),
-                child: TextField(
-                  style: TextStyle(fontFamily: 'RobotoMono'),
-                  controller: editingController,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
+                Text(
+                  'Enter Text',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'RobotoMono',
                   ),
-                  onSubmitted: (val) {
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Container(
+                  width: _size.width,
+                  height: 50.0,
+                  padding: EdgeInsets.only(left: 10.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4.0),
+                      border: Border.all(
+                        color: Colors.black,
+                      )),
+                  child: TextField(
+                    style: TextStyle(fontFamily: 'RobotoMono'),
+                    controller: editingController,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                    onSubmitted: (val) {
+                      setState(() {
+                        displayText = editingController.text;
+                      });
+                      animationController.reset();
+                      animationController.forward();
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    FocusScope.of(context).unfocus();
                     setState(() {
                       displayText = editingController.text;
                     });
+                    animationController.reset();
+                    animationController.forward();
                   },
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    displayText = editingController.text;
-                  });
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.symmetric(horizontal: 8.0),
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12.0),
-                    color: Colors.blue,
-                  ),
-                  child: Text(
-                    'Display',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+                  child: Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.0),
+                      color: Colors.blue,
+                    ),
+                    child: Text(
+                      'Display',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              GradientText(
-                displayText,
-                style: TextStyle(
-                  fontSize: 30.0,
-                  letterSpacing: 2.0,
-                  fontFamily: 'Anton',
+                SizedBox(
+                  height: 30,
                 ),
-                colors: [
-                  Colors.blue,
-                  Colors.red,
-                  Colors.teal,
-                ],
-              ),
-            ],
+                SlideTransition(
+                  position: Tween<Offset>(
+                    begin: Offset(-_size.width, 0),
+                    end: Offset(
+                      0,
+                      0,
+                    ),
+                  ).animate(animation),
+                  child: ShaderMask(
+                    shaderCallback: (Rect bounds) {
+                      return LinearGradient(
+                        colors: [
+                          Colors.blue,
+                          Colors.red,
+                          Colors.teal,
+                        ],
+                      ).createShader(bounds);
+                    },
+                    child: Text(
+                      displayText,
+                      style: TextStyle(
+                        fontSize: 50.0,
+                        color: Colors.white,
+                        fontFamily: 'Allison',
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
